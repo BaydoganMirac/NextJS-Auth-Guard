73f7e6a5a121af537853abc4d2e655d59c975a3b
Miraç Baydoğan <baydoganmirac@gmail.com>
2022-04-15 01:29:42 +0300

    fix logout bug

--- pages/profile.js
+++ pages/profile.js
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import AuthGuard from "../services/AuthGuard";
 import Link from 'next/link';
+import { userService } from "../services/userService";
 export default function Profile(){
     // AuthGuardı bir component misali kullanıcıya göstermek istemediğimiz sayfaya ekliyoruz. 
     return (<>
@@ -8,7 +9,7 @@ export default function Profile(){
             <Navbar></Navbar>
             <div className='container home-section'>
                 <div>
-                This area is protected. If you are logged in, you can access this page. You can logout from the <Link href='/login'><a style={{color: `red`, marginLeft: '5px'}}>link</a></Link>
+                This area is protected. If you are logged in, you can access this page. You can logout from the <Link href='#'><a onClick={()=>{userService.logout()}} style={{color: `red`, marginLeft: '5px'}}>link</a></Link>
                 </div>
             </div>
         </AuthGuard>
